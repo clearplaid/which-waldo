e4d8b30f06c86b7db05444cbbbde87e5beecc50c
clearplaid <clearplaid@gmail.com>
2019-07-25 19:54:30 -0400

    Updates

---
+++ precache-manifest.c1b09e79ff9e8ff340748945d07a126e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26da76084301706f3ba8f69906a24e4c",
+    "url": "/which-waldo/index.html"
+  },
+  {
+    "revision": "32302d104de387071aba",
+    "url": "/which-waldo/static/css/main.bb80645b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/which-waldo/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "32302d104de387071aba",
+    "url": "/which-waldo/static/js/main.1f906d81.chunk.js"
+  },
+  {
+    "revision": "f3aabf381178eecfc194",
+    "url": "/which-waldo/static/js/runtime~main.d6c282a9.js"
+  }
+]);
